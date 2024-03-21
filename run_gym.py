bf546ab82452e74867c27ca6d4c1b9897e971190
KevinFham <kevin.pha01@gmail.com>
2024-03-20 22:50:22 -0500

    ir

--- run_gym.py
+++ run_gym.py
@@ -8,6 +8,7 @@ class args:
     GRAIN = 10
     BOT_POPULATION = 2
     BOT_SPAWN_RANGE = 5.0
+    FLOCKINGBOT_ASSET_DIR = "/World/flockingbot"
 
 from omni.isaac.kit import SimulationApp
 simulation_app = SimulationApp({"headless": args.SIM_HEADLESS})
@@ -17,6 +18,7 @@ from omni.isaac.core import World
 from omni.isaac.wheeled_robots.controllers import DifferentialController
 from omni.isaac.wheeled_robots.robots import WheeledRobot
 from omni.isaac.sensor import IMUSensor
+from omni.isaac.range_sensor._range_sensor import acquire_ultrasonic_sensor_interface
 from omni.isaac.core.objects import FixedCuboid, VisualCuboid
 #from omni.isaac.core.prims.rigid_prim_view import RigidPrimView
 import omni.isaac.core.utils.prims as prim_utils
@@ -66,10 +68,10 @@ for i, spawn in enumerate(spawns[-2:]):
     )
 
 # Spawn robot
-add_reference_to_stage(usd_path="/home/kevin/Desktop/flockingeaglesisaacsim/flockingbot_script.usd", prim_path="/World/flockingbot")
+add_reference_to_stage(usd_path="/home/kevin/Desktop/flockingeaglesisaacsim/flockingbot_script.usd", prim_path=args.FLOCKINGBOT_ASSET_DIR)
 diff = DifferentialController(name="flockingbot_diff", wheel_radius=0.03, wheel_base=0.1125)
 robot = WheeledRobot(
-    prim_path="/World/flockingbot", 
+    prim_path=args.FLOCKINGBOT_ASSET_DIR, 
     usd_path="/home/kevin/Desktop/flockingeaglesisaacsim/flockingbot_script.usd", 
     wheel_dof_names=["left_wheel_joint", "right_wheel_joint"], 
     wheel_dof_indices=[0,1], 
@@ -88,7 +90,7 @@ class FlockingBot:
         self.robot = robot
         self.robot.initialize()
         self.diff = diff
-        self.imu = IMUSensor(prim_path="/World/flockingbot/chassis/Imu_Sensor")
+        self.imu = IMUSensor(prim_path=args.FLOCKINGBOT_ASSET_DIR + "/chassis/Imu_Sensor")
         self.imu.initialize()
     def normalize_angle(self, angle): return angle + np.pi * 2 if angle < 0.0 else angle
     def quaternion_to_euler_x(self, quat):
@@ -97,6 +99,8 @@ class FlockingBot:
     def get_position(self): return self.robot.get_world_pose()[0]
     def get_orientation_quat(self): return self.imu.get_current_frame()['orientation']
     def get_orientation_euler_x(self): return self.quaternion_to_euler_x(self.get_orientation_quat())
+    def get_ir_reading(self, ir): return acquire_ultrasonic_sensor_interface().get_depth_data(args.FLOCKINGBOT_ASSET_DIR + f'/chassis/US_sensors/UltrasonicArray_{ir}', 0) / 1000
+    def get_all_ir_readings(self): return [self.get_ir_reading(0), self.get_ir_reading(1), self.get_ir_reading(2)]
     def get_heading_angle_rad(self, dest_pos):
         position_diff = np.subtract(self.get_position(), dest_pos)
         heading_angle = np.fmod(-np.arctan2(position_diff[1], position_diff[0]), np.pi * 2)
@@ -127,6 +131,7 @@ while simulation_app.is_running():
         flockingbot.go_to_position(route[route_idx])
         if flockingbot.get_distance_to_destination(route[route_idx]) < 0.2:
             route_idx = min(len(route) - 1, route_idx + 1)
+        print(flockingbot.get_all_ir_readings())
         
         world.step(render=True)
         frame_idx += 1
